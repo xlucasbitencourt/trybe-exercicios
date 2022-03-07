a554e5a3e41777628def8bbea39adf2ac58314b0
Lucas Bitencourt <lblukz@aol.com>
2022-03-07 16:22:52 -0300

    Inclusão de exercício switch

---
+++ Modulo1/bloco-4/04.1/Prática/switch.js
@@ -0,0 +1,19 @@
+let trafficLight = 'Vermelho';
+switch (trafficLight) {
+    case 'vermelho':
+        console.log('Pare');
+        break;
+
+    case 'amarelo':
+        console.log('Atenção');
+        break;
+
+    case 'verde':
+        console.log('Siga');
+        break;
+
+    default:
+        console.log('Valor não identificado');
+}
+
+//case sensitive!

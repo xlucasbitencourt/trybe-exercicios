fd114a3dc1610e275be9e606acd47ac896eadd19
Lucas Bitencourt <lblukz@aol.com>
2022-03-09 16:41:22 -0300

    Exercícios bônus

---
+++ Modulo1/bloco-4/04.3/bonus.js
@@ -0,0 +1,30 @@
+// n = 5
+
+// *****
+// *****
+// *****
+// *****
+// *****
+console.log('Exercício 1:');
+let n = 5;
+let resultado = ''
+for(let index = 1; index <=n; index ++) {
+    resultado = resultado + '*';
+}
+for(let index = 1; index <=n; index ++) {
+    console.log(resultado);
+}
+
+
+//*
+//**
+//***
+//****
+//*****
+console.log('Exercício 2: ');
+resultado = '';
+for (let index = 1; index <= n; index++){
+    resultado = resultado + '*';
+    console.log(resultado);
+}
+

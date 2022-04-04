bade72b8c22fe6c36b8f11826bda9203bbb25076
Lucas Bitencourt <lblukz@aol.com>
2022-04-04 19:28:08 -0300

    Parte de conteúdos

---
+++ Modulo 1/Bloco 7/07.2/conteúdos.js
@@ -0,0 +1,83 @@
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
+
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+console.log(sum(2, 3));
+//-----------------------------------------------------------------
+
+const recebe = (obj, nom, ch) => {
+  const objeto = obj = {};
+  objeto[nom] = ch;
+  return obj;
+}
+
+console.table(recebe('dados', 'nome', 'lucas'));
+
+//--------------------------------------------------------------------
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for (const index in arrayOfSkills) {
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
+
+//-----------------------------------------------------------------------------------
+
+var obj = { foo: 'bar', baz: 42 };
+var map = new Map(Object.entries(obj));
+console.log(map); // Map { foo: "bar", baz: 42 }
+
+//---------------------------------------------------------------------------
+
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
+

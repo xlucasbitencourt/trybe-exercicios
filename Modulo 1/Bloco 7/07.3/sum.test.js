1e009a34127ad1a3877caea1d71ce54ecb1218b6
Lucas Bitencourt <lblukz@aol.com>
2022-04-06 14:06:27 -0300

    Início exercícios

---
+++ Modulo 1/Bloco 7/07.3/sum.test.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@jest/globals');
+const sum = require('./sum');
+
+test('Teste se o retorno de sum(4, 5) é 9', () => {
+    expect(sum(4, 5)).toBe(9);
+    
+});
+
+test('Teste se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).toBe(0);
+})

ac57a2b82bfb88c6768d18138bd8efaa7fac827b
Lucas Bitencourt <lblukz@aol.com>
2022-04-11 19:46:17 -0300

    Exercícios

---
+++ Modulo 1/Bloco 8/08.3/exercícios.js
@@ -0,0 +1,141 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // Adicione o código do exercício aqui:
+
+// 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+function formatedBookNames() {
+    // escreva seu código aqui
+    return books.map((dados) => `${dados.name} - ${dados.genre} - ${dados.author.name}`);
+}
+
+// console.log(formatedBookNames());
+
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+function nameAndAge() {
+    // escreva seu código aqui
+    
+    const lista =  books.map((da) => {
+        return {
+            autor: da.author.name,
+            idade: da.releaseYear - da.author.birthYear,
+        };
+    });
+    return lista.sort((a, b) => a.idade - b.idade);
+}
+
+// console.log(nameAndAge());
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+function fantasyOrScienceFiction() {
+    // escreva seu código aqui
+    return books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
+}
+
+// console.log(fantasyOrScienceFiction());
+
+// 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+
+function oldBooksOrdered() {
+    // escreva seu código aqui
+    const maisDe60 = books.filter((book) => book.releaseYear < 1962);
+    return maisDe60.sort((a, b) => a.releaseYear - b.releaseYear);
+}
+
+// console.log(oldBooksOrdered());
+
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+function fantasyOrScienceFictionAuthors() {
+    // escreva seu código aqui
+    const selecao = books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia').map((book) => book.author.name).sort();
+    return selecao;
+}
+
+// console.log(fantasyOrScienceFictionAuthors());
+
+// 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+function oldBooks() {
+    // escreva seu código aqui
+    const velhos = books.filter((book) => book.releaseYear < 1962);
+    return velhos.map((velho) => velho.name);
+}
+
+// console.log(oldBooks());
+
+// 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+function authorWith3DotsOnName() {
+    // escreva seu código aqui
+    return books.filter((book) => (
+        book.author.name[1] === '.'
+        && book.author.name[4] === '.'
+        && book.author.name[7] === '.'
+      ))[0].name;
+}
+
+// console.log(authorWith3DotsOnName());

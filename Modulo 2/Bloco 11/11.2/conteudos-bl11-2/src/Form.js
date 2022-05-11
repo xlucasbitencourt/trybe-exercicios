16addd21ddd189f5f43a694f90b0af0a71f9e354
Lucas Bitencourt <lblukz@aol.com>
2022-05-11 15:26:54 -0300

    Event handlers genéricos

--- Modulo 2/Bloco 11/11.2/conteudos-bl11-2/src/Form.js
+++ Modulo 2/Bloco 11/11.2/conteudos-bl11-2/src/Form.js
@@ -1,41 +1,78 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import './Form.css'
 
 class Form extends Component {
-  constructor() {
+  /*constructor() {
     super();
 
-    this.handleChange = this.handleChange.bind(this);
-
     this.state = {
-      estadoFavorito: '',
+      estadoFavorito: "",
+      vaiComparecer: false,
+      idade: 0,
     };
-  }
+  }*/
 
+  state = {
+    // declara o state fora do constructor desta forma
+    estadoFavorito: "",
+    vaiComparecer: false,
+    idade: 0,
+  };
 
-  handleChange(event) {
+  mudaForm = ({ target }) => {
+    // não precisa declarar no constructor quando é arrow function
+    const { name } = target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
     this.setState({
-      estadoFavorito: event.target.value,
+      [name]: value,
     });
-  }
+  };
 
   render() {
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
-        <form className="form">
+        <form className='form'>
+          <fieldset>
+            <legend>Título</legend>
+            <label>
+              Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
+              <textarea
+                name='estadoFavorito'
+                value={this.state.estadoFavorito}
+                onChange={this.mudaForm}
+              />
+            </label>
+          </fieldset>
+          <br />
           <label>
-            Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+            Qual sua idade?
+            <input
+              type='number'
+              name='idade'
+              value={this.state.idade}
+              onChange={this.mudaForm}
+            />
           </label>
-          <input
-            type="number"
-            name="idade"
-          />
-          <input
-            type="checkbox"
-            name="vaiComparecer"
-          />
+          <br />
+          <label>
+            Vai comparecer?
+            <input
+              type='checkbox'
+              name='vaiComparecer'
+              value={this.state.vaiComparecer}
+              onChange={this.mudaForm}
+            />
+          </label>
+          <br />
+          <label>
+            Insira seu arquivo
+            <input 
+              type='file'
+              name="arquivo"
+            />
+          </label>
         </form>
       </div>
     );
